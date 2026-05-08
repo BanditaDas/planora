bc7ca0290041d1388331865ecdcb29a44b98ff52
Bandita Das <dbandita362@gmail.com>
2026-05-08 12:23:14 +0530

    close btn added

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 var allelem = document.querySelectorAll('.elem')
 var fullelem = document.querySelectorAll ('.fullelem')
+var backbtn = document.querySelectorAll('.fullelem .back')
 
 allelem.forEach(function(elem){
     elem.addEventListener('click', function(){
@@ -9,3 +10,13 @@ allelem.forEach(function(elem){
     })
     
 })
+ 
+backbtn.forEach(function(back){
+    back.addEventListener('click', function(){
+        
+    fullelem[back.id].style.display = 'none'
+        
+    })
+})    
+    
+ 
